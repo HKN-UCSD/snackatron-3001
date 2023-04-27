d88fad2dc54e03d8090945dda0d462bf6df75f7b
Adneths <bill.lui0921@gmail.com>
2023-04-27 14:52:28 -0700

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="panels">
+        <div id="info"></div>
+        <div id="menu"></div>
+        <div id="cart">
+          <div id="cart-list">Cart</div>
+          <div id="checkout">Checkout</div>
+        </div>
+      </div>
     </div>
   );
 }
