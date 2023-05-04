90b026405c26bf56d06f4a649301786cfcd749e5
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2023-05-04 15:39:24 -0700

    Andrew updated UI

--- src/App.js
+++ src/App.js
@@ -6,10 +6,53 @@ function App() {
     <div className="App">
       <div id="panels">
         <div id="info"></div>
-        <div id="menu"></div>
+        <div id="menu">
+            <h1 class="title">Prices</h1>
+            <table id="price-table" frame="hsides" rules="rows">
+                <tr>
+                    <h1 class="category">Snacks</h1>
+                </tr>
+                <tr>
+                    <td>Chicken Bake</td>
+                    <td>$1.00</td>
+                    <td>
+                        <button class="add">+</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Chicken Bake</td>
+                    <td>$1.00</td>
+                    <td>
+                        <button class="add">+</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
         <div id="cart">
-          <div id="cart-list">Cart</div>
-          <div id="checkout">Checkout</div>
+          <div id="cart-list">
+            <h1 class="title">Cart</h1>
+            <table id="cart-table" frame="hsides" rules="rows">
+                <tr>
+                    <td>Chicken Bake</td>
+                    <td>$1.00</td>
+                    <td>
+                        <button class="trash">
+                            <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Chicken Bake</td>
+                    <td>$1.00</td>
+                    <td>
+                        <button class="trash">
+                            <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                        </button>
+                    </td>
+                </tr>
+            </table>
+          </div>
+          <button id="checkout">Checkout</button>
         </div>
       </div>
     </div>
