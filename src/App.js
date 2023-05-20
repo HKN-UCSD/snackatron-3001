c83f78b5e7df824f91b8a644e17ee72f86c61caa
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2023-05-19 17:42:53 -0700

    Andrew finished html/css outline

--- src/App.js
+++ src/App.js
@@ -5,52 +5,259 @@ function App() {
   return (
     <div className="App">
       <div id="panels">
-        <div id="info"></div>
+        <div id="info">
+            <img id="logo" src="https://hkn.ucsd.edu/wp-content/uploads/2014/11/HKN-Logo-White.png"></img>
+            <p id="instructions">Scan the barcode of your items :)</p>
+            <p id="ty">Made by HKN Dev Team © 2023</p>
+        </div>
         <div id="menu">
             <h1 class="title">Prices</h1>
-            <table id="price-table" frame="hsides" rules="rows">
-                <tr>
-                    <h1 class="category">Snacks</h1>
-                </tr>
-                <tr>
-                    <td>Chicken Bake</td>
-                    <td>$1.00</td>
-                    <td>
-                        <button class="add">+</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Chicken Bake</td>
-                    <td>$1.00</td>
-                    <td>
-                        <button class="add">+</button>
-                    </td>
-                </tr>
-            </table>
+            <hr></hr>
+            <div id="prices">
+                <h1 class="category">Snacks</h1>
+                <table class="price-table" frame="hsides" rules="rows">
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                </table>
+                <h1 class="category">Drinks</h1>
+                <table class="price-table" frame="hsides" rules="rows">
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                </table>
+                <h1 class="category">Stuff</h1>
+                <table class="price-table" frame="hsides" rules="rows">
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                </table>
+                <h1 class="category">Stuff Again</h1>
+                <table class="price-table" frame="hsides" rules="rows">
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="add">+</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
         <div id="cart">
           <div id="cart-list">
             <h1 class="title">Cart</h1>
-            <table id="cart-table" frame="hsides" rules="rows">
-                <tr>
-                    <td>Chicken Bake</td>
-                    <td>$1.00</td>
-                    <td>
-                        <button class="trash">
-                            <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Chicken Bake</td>
-                    <td>$1.00</td>
-                    <td>
-                        <button class="trash">
-                            <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
-                        </button>
-                    </td>
-                </tr>
-            </table>
+            <hr></hr>
+            <div id="cart-items">
+                <table id="cart-table" frame="void" rules="rows">
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Chicken Bake</td>
+                        <td>$1.00</td>
+                        <td>
+                            <button class="trash">
+                                <img class="trash-img" src="https://cdn-icons-png.flaticon.com/512/1843/1843344.png"></img>
+                            </button>
+                        </td>
+                    </tr>   
+                </table>
+            </div>
+            
           </div>
           <button id="checkout">Checkout</button>
         </div>
