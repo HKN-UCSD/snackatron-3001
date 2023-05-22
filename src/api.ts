a3d47bc1001a6f5df952338876370afbb69fe102
Adneths <bill.lui0921@gmail.com>
2023-05-22 11:40:04 -0700

    Add api return types
    Getting error in ide
    TS270 (TS) An async function or method in ES5/ES3 requires the 'Promise' constructor.  Make sure you have a declaration for the 'Promise' constructor or include 'ES2015' in your '--lib' option.

--- src/api.ts
+++ src/api.ts
@@ -7,27 +7,41 @@ interface Order {
   email: string;
   cart: Item[];
 }
+interface Menu {
+  tags: string[];
+  ids: string[];
+  [values: string]: string[];
+}
+interface Reciept {
+  orderNumber: string;
+  finalDebt: number;
+  total: number;
+  time: string;
+}
+interface Result {
+  success: boolean;
+}
 
-export async function getMenu() {
+export async function getMenu(): Promise<Menu | Result> {
   return fetch('/menu', {
     method: 'GET',
-  }).then((res) => res.json()).catch((err) => console.log(err));
+  }).then((res) => res.json()).catch((err) => { console.log(err); return { success: false }; });
 }
-export async function refreshMenu() {
+export async function refreshMenu(): Promise<Result> {
   return fetch('/menu', {
     method: 'POST',
     headers: {
       "Content-type": "application/json",
     }
-  }).then((res) => res.json()).catch((err) => console.log(err));
+  }).then((res) => res.json()).catch((err) => { console.log(err); return { success: false}; });
 }
 
-export async function checkout(order : Order) {
+export async function checkout(order: Order): Promise<Reciept | Result> {
   return fetch('/transaction', {
     method: 'POST',
     body: JSON.stringify(order),
     headers: {
       "Content-type": "application/json",
     }
-  }).then((res) => res.json()).catch((err) => console.log(err));
+  }).then((res) => res.json()).catch((err) => { console.log(err); return { success: false }; });
 }
